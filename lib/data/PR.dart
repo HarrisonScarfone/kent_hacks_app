8eb3d471b7c3dafd7cc19134e55dd3dac8ef65b4
Vickensteiner <victorjmarton.vjm@gmail.com>
2019-09-29 00:45:34 -0400

    partway done profile screen design

---
+++ lib/data/PR.dart
@@ -0,0 +1,8 @@
+class PR{
+  final String title;
+  final int recordNumber;
+  final String unit;
+  final String category;
+
+  PR(this.title, this.recordNumber, this.unit, this.category);
+}
---
+++ lib/data/PRsList.dart
@@ -0,0 +1,26 @@
+import 'PR.dart';
+
+class PRsList {
+  static List<PR> PRs = [PR('Fasted ball bowled in a test match', 0, 'Kilometers per hour', 'Human Achivements'),
+    PR('Fasted  balloon dog made behind back', 0, 'Seconds', 'Hobbies and Receation'),
+    PR('Highest annual earnings for a boxer', 0, 'USD', 'Jobs and Work'),
+    PR('Heaviest carrot grown', 0, 'Kilograms', 'Food and Drink'),
+    PR('Most drink cans crushed with elbow in one minute', 0, 'Cans', 'Human Achievements'),
+    PR('Largest house of cards', 0, 'Stories', 'Structure/Arts and Craft'),
+    PR('Largest collection of bi-metallic coins', 0, 'Coins', 'Dedication and Collections'),
+    PR('Longest escalator ride', 0, 'Miles', 'Dedication and Collections'),
+    PR('Most wins of the FIFA World Cup by a player', 0, 'Cups', 'Human Achievements'),
+    PR('Most one finger push ups in 30 seconds', 0, 'Push ups', 'Human Achievements'),
+    PR('Most functional gadgets in a cosplay suit', 0, 'Gadgets', 'Hobbies and Recreation'),
+    PR('Oldest gymanst', 0, 'Years', 'Human Achievements'),
+    PR('Longest career as an ice cream man', 0, 'Years', 'Jobs and Work'),
+    PR('Longest joke-telling marathon', 0, 'Jokes in 24 hours', 'Hobbies and Recreation'),
+    PR('Tallest structure built with LEGO bricks', 0, 'Meters', 'Structure/Arts and Craft'),
+    PR('Longest French knitting', 0, 'Kilometers', 'Structure/Arts and Craft'),
+    PR('Largest pizza made', 0, 'Square meters', 'Food and Drink'),
+    PR('Oldest hedge maze', 0, 'Year made', 'Structure/Arts and Craft'),
+    PR('Most romance novels penned', 0, 'Novels', 'Jobs and Work'),
+    PR('Most piercings in a lifetime', 0, 'Piercings', 'Human Achievements'),
+    PR('Most human targets hi with plungers in one minute', 0, 'Person/People', 'Human Achievements'),
+  ];
+}
